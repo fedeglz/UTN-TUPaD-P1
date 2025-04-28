a6a9b5f8d5ce9c4cd6631ecfefc4bf6d2bf250ba
federico gonzalez <fedeg130@gmail.com>
2025-04-28 11:25:46 -0300

    se agrego la carpeta Listas, y tambien se subio el tp-Listas

---
+++ 05 Listas/TP-Listas.py
@@ -0,0 +1,67 @@
+#González Federico
+#34605567
+
+
+#Ejercicio 1
+lista_numeros = list(range(4, 101, 4))
+print(lista_numeros)
+
+
+#Ejercicio 2
+lista = ["Perro", 2, 3.14, True, "Gato"]
+penultimo_elemento = lista[-2]
+print(penultimo_elemento)
+
+
+#Ejercicio 3
+lista_vacia = []
+
+lista_vacia.append(1)
+lista_vacia.append("Perro")
+lista_vacia.append(True)
+print(lista_vacia)
+
+
+#Ejercicio 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
+
+
+#Ejercicio 5
+#Elimina el numero mas grande de la lista
+
+
+#Ejercicio 6
+lista_numeros = list(range(10, 31, 5))
+primero_dos_numeros = lista_numeros[:2]
+print(primero_dos_numeros)
+
+
+#Ejercicio 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "golf"
+autos[2] = "206"
+print(autos)
+
+
+#Ejercicio 8
+dobles = []
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+print(dobles)
+
+
+#Ejercicio 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+
+#Ejercicio 10
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
